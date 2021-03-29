cf2435bf7a5eefafbd1d0afc864a740cb98a95da
Jidim <diimmiiiifififu@gmail.com>
2021-03-29 13:40:52 +0300

    Убрал scanf

--- app/main.c
+++ app/main.c
@@ -5,8 +5,7 @@
 int main()
 {
     printf("Hello World!\nВведите коэффиценты A, B, C\n");
-    double a, b, c;
-    scanf("%lf %lf %lf", &a, &b, &c);
+    double a=0, b=0, c=0;
     square(a, b, c);
     myfunc(2);
     return 0;
