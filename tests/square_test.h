231d09936f6525a46febfadf16d72577c59ac533
Jidim <diimmiiiifififu@gmail.com>
2021-04-05 14:57:18 +0300

    Количество ответов

--- tests/square_test.h
+++ tests/square_test.h
@@ -7,7 +7,6 @@ extern "C" {
 #include "square.h"
 }
 
-// Стандартные ситуации
 TEST(square, no_sol) {
     solution res = square(1, 2, 6);
     EXPECT_EQ(0, res.count);
@@ -15,23 +14,26 @@ TEST(square, no_sol) {
 TEST(square, one_sol) {
     solution res = square(4, 4, 1);
     EXPECT_EQ(-0.5, res.x1);
+    EXPECT_EQ(1, res.count);
 }
 
 TEST(square, two_sol) {
     solution res = square(2, 5, 2);
     EXPECT_EQ(-2, res.x1);
     EXPECT_EQ(-0.5, res.x2);
+    EXPECT_EQ(2, res.count);
 }
-// Один корень
 TEST(square, test4_negative){
     solution res = square(0, 1, 1);
     EXPECT_EQ(-1, res.x1);
+    EXPECT_EQ(1, res.count);
 
 }
 TEST(square, test5_negative){
     solution res = square(1, 0, -1);
     EXPECT_EQ(-1, res.x1);
     EXPECT_EQ(1, res.x2);
+    EXPECT_EQ(2, res.count);
 
 }
 TEST(square, test6_negative){
@@ -40,31 +42,27 @@ TEST(square, test6_negative){
 
 }
 
-TEST(square, test8_negative){
+TEST(square, test7_negative){
     solution res = square(0, 1, 0);
     EXPECT_EQ(0, res.x1);
-
+    EXPECT_EQ(1, res.count);
 }
-TEST(square, test10_negative){
+TEST(square, test8_negative){
     solution res = square(1, 1, 0);
     EXPECT_EQ(-1, res.x1);
     EXPECT_EQ(0, res.x2);
-
+    EXPECT_EQ(2, res.count);
 }
-TEST(square, test11_negative){
+TEST(square, test9_negative){
     solution res = square(1, 0, 0);
     EXPECT_EQ(0, res.x1);
-
-
+    EXPECT_EQ(1, res.count);
 }
-// Нет корней
-TEST(square, test7_negative){
+TEST(square, test100_negative){
     solution res = square(0, 0, 1);
     EXPECT_EQ(0, res.count);
 }
-// Бескоонечное количество корней
-TEST(square, test9_negative){
+TEST(square, test11_negative){
     solution res = square(0, 0, 0);
-    EXPECT_EQ(0, res.count);
-
+    EXPECT_EQ(3, res.count);
 }
